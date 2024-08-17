3916266a52cff312e4669f293aea603cb58f0715
Bruno Oliveira <brunoroliveira7@gmail.com>
2024-08-17 16:24:54 -0300

    checking values ​​out of range

--- main.js
+++ main.js
@@ -1,39 +1,67 @@
-const randomNumber = Math.round(Math.random() * 10);
+let randomNumber = Math.round(Math.random() * 10);
 let xAttempts = 1;
 
 const screen1 = document.querySelector(".screen1");
 const screen2 = document.querySelector(".screen2");
 
-//função callback
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+
+// eventos
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", handleResetClick);
+document.addEventListener("keydown", pressEnter);
+
+// funções callback
 function handleTryClick(event) {
   event.preventDefault(); //não faça o padrão(não envia o form btn)
 
   const inputNumber = document.querySelector("#inputNumber");
 
+  if (Number(inputNumber.value) === "") {
+    return;
+  }
+
   if (Number(inputNumber.value) == randomNumber) {
-    screen1.classList.add("hide");
-    screen2.classList.remove("hide");
+    toggleScreen();
 
     screen2.querySelector(
       "h2"
     ).innerText = `Acertou em ${xAttempts} tentativas`;
+    // Verifica se está fora do intervalo
+  } else if (Number(inputNumber.value) < 0 || Number(inputNumber.value) > 10) {
+    alert("Número fora do intervalo entre 0 a 10");
   }
 
   inputNumber.value = "";
   xAttempts++;
 }
 
-//eventos
-const btnTry = document.querySelector("#btnTry");
-const btnReset = document.querySelector("#btnReset");
+function handleResetClick() {
+  toggleScreen();
+  xAttempts = 1;
+  randomNumber = Math.round(Math.random() * 10); //reset o randomNumber
+}
 
-/* addEventListener-função que adiciona duas coisas: um envento click, 
+function toggleScreen() {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+}
+
+function pressEnter(e) {
+  if (e.key == "Enter" && screen1.classList.contains("hide")) {
+    handleResetClick();
+  }
+}
+
+/* 
+addEventListener-função que adiciona duas coisas: um envento click, 
 e uma função que é argumento da primeira, executada depois
-(callback-somente depois do click) */
+(callback-somente depois do click) 
 btnTry.addEventListener("click", handleTryClick);
-btnReset.addEventListener("click", function () {
-  //func anônima (callback), outra forma
+btnReset.addEventListener("click", function () { //func anônima (callback), outra forma
   screen1.classList.remove("hide");
   screen2.classList.add("hide");
   xAttempts = 1;
 });
+*/
